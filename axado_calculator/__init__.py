cd452b2c5da0569ca2a8cdfa2ac12282c895d54e
Alexandre Proença <alexandre.proenca@hotmail.com.br>
2017-03-05 12:53:32 -0300

    Bump version: 1.1.2 → 1.1.3

--- axado_calculator/__init__.py
+++ axado_calculator/__init__.py
@@ -28,7 +28,7 @@ _/~~~~~~~~|~~\,   ---|---\___/----|  \/\-\
 """
 
 __title__ = 'Axado Challenge'
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 __author__ = 'Alexandre Proença'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 Alexandre Proença'
