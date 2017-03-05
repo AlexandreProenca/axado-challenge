31ce3f08cf69e3e2ee2a175b9cd68c09f8e400ad
Alexandre Proença <alexandre.proenca@hotmail.com.br>
2017-03-05 12:56:50 -0300

    Hot fix

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'axado=axado_claculator.axado_calculator:main',
+            'axado=axado_calculator.axado_calculator:main',
         ],
     },
     zip_safe=False,
